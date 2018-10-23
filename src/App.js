08d0682a2531a68d657a28929e089f5ec8ce4261
Jeff Ong <jeffrey.ong@frogdesign.com>
2018-10-23 17:13:52 -0400

    sequencer demo

--- src/App.js
+++ src/App.js
@@ -26,30 +26,44 @@ class App extends Component {
       "fadeOut": "32n"
     }).toMaster();
 
-    this.sampleNames = ['Kick', 'HH', 'Snare'];
-    this.matrix = new Nexus.Sequencer('#target', {
-      'size': [600, 400],
+    this.sampleNames = ['HH', 'Snare', 'Kick'];
+    this.nx = new Nexus.Sequencer('#target', {
       'mode': 'toggle',
       'rows': 3,
-      'columns': 10
+      'columns': 16
     });
 
-		//this.loop = new Tone.Sequence(function(time, col){
-		//	let column = matrix1.matrix[col];
-		//	for (let i = 0; i < 3; i++){
-		//		if (column[i] === 1){
-		//				//slightly randomized velocities
-		//				let vel = Math.random() * 0.5 + 0.5;
-		//				this.drums.get(this.sampleNames[i]).start(time, 0, "32n", 0, vel);
-		//		}
-		//	}
-		//}, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
+    this.loop = new Tone.Sequence((time, col) => {
+      try {
+        for (let i = 0; i < 3; i++){
+          let column = this.nx.matrix.pattern;
+          if (column[i][col] === true){
+            //slightly randomized velocities
+            let vel = Math.random() * 0.5 + 0.5;
+            this.drums.get(this.sampleNames[i]).start(time, 0, "16n", 0, vel);
+          }
+        }
+      } catch (err){
+        console.log('error')
+      }
+		}, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
+
+    this.transport.start();
+    this.loop.start();
   }
   
   render() {
     return (
-      <div className="App">
-        <div id="target"></div>
+      <div styleName="App">
+        <code styleName='header'>beat rockers sequencer demo</code>
+        <div styleName='sequencer'>
+          <div>
+            <div styleName='sample'>hh</div>
+            <div styleName='sample'>snare</div>
+            <div styleName='sample'>kick</div>
+          </div>
+          <div id="target" styleName='target'></div>
+        </div>
       </div>
     );
   }
